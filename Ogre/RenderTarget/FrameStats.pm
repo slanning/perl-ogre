f798886099055b88423546d31c1a7245898ec2ad
Scott Lanning <slanning@cpan.org>
2013-06-25 22:08:51 +0200

    fix Ogre::RenderTarget::FrameStats

--- Ogre/RenderTarget/FrameStats.pm
+++ Ogre/RenderTarget/FrameStats.pm
@@ -5,3 +5,29 @@ use warnings;
 
 
 1;
+
+__END__
+=head1 NAME
+
+Ogre::RenderTarget::FrameStats
+
+=head1 SYNOPSIS
+
+  use Ogre;
+  use Ogre::RenderTarget::FrameStats;
+  # (for now see examples/README.txt)
+
+=head1 DESCRIPTION
+
+See the online API documentation at
+ L<http://www.ogre3d.org/docs/api/html/classOgre_1_1RenderTarget_1_1FrameStats.html>
+
+B<Note:> this Perl binding is currently I<experimental> and subject to API changes.
+
+=head1 AUTHOR
+
+Scott Lanning E<lt>slanning@cpan.orgE<gt>
+
+For licensing information, see README.txt .
+
+=cut
